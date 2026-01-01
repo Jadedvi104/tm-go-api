8ba00a0591e1e3e37418fc20e0024433a9be1fe7
jadedvi104 <jadedvi104@gmail.com>
2026-01-01 22:58:19 +0700

    Correct version of DB connect

--- database/database.go
+++ database/database.go
@@ -5,9 +5,9 @@ import (
 	"log"
 	"os"
 
-	"my-fiber-app/models"
+	"tm-go-api/models"
 
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlserver" // <--- CHANGED FROM postgres
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
@@ -19,27 +19,38 @@ type DbInstance struct {
 var Database DbInstance
 
 func ConnectDb() {
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
+	// dsn := fmt.Sprintf(
+	// 	"host=%s user=%s password=%s database=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
+	// 	os.Getenv("DB_HOST"),
+	// 	os.Getenv("DB_USER"),
+	// 	os.Getenv("DB_PASSWORD"),
+	// 	os.Getenv("DB_NAME"),
+	// 	os.Getenv("DB_PORT"),
+	// )
+	username := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	server := os.Getenv("DB_HOST")
+	port := 1433
+	database := os.Getenv("DB_NAME") // <--- CRITICAL: Must match exactly
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+	// 2. Build the DSN
+	// Note: 'encrypt=true' is required for Azure.
+	// Note: 'database=' tells it NOT to look in master.
+	dsn := fmt.Sprintf("sqlserver://%s:%s@%s:%d?database=%s&encrypt=true",
+		username, password, server, port, database)
+
+	// Note: We use sqlserver.Open() instead of postgres.Open()
+	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
 
 	if err != nil {
-		log.Fatal("Failed to connect to database. \n", err)
+		log.Fatal("Failed to connect to Azure SQL. \n", err)
 	}
 
-	log.Println("connected")
+	log.Println("connected to Azure SQL")
 	db.Logger = logger.Default.LogMode(logger.Info)
 
-	// Auto Migrate the schema
 	log.Println("running migrations")
 	db.AutoMigrate(&models.User{})
 
